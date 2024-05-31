46cc9bac39c49c347450d7f8627869f0a582a086
HoyeongJeon <hoyeong0302@gmail.com>
2024-05-31 18:25:31 +0900

    feat: 신고 구현

--- src/components/ChatRoom/index.tsx
+++ src/components/ChatRoom/index.tsx
@@ -1,12 +1,26 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { useParams } from "react-router-dom";
-import { FormEvent, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { useStore } from "@/stores/store";
 import useSocket from "@/hooks/useSocket";
+import { AiFillAlert } from "react-icons/ai";
 
 interface ChatMessageProps {}
+interface ChatParterProps {
+  id: number;
+  nickname: string;
+}
 
 const ChatRoom = ({}: ChatMessageProps) => {
   const { id: userId } = useStore((state) => state);
@@ -14,10 +28,58 @@ const ChatRoom = ({}: ChatMessageProps) => {
   const [socket, disconnect] = useSocket(Number(roomId));
   const [message, setMessage] = useState<string>("");
   const [messages, setMessages] = useState<any[]>([]);
-  const [chatParter, setChatPartner] = useState<string>("");
+  const [chatParter, setChatPartner] = useState<ChatParterProps>();
+  const [checkedReason, setCheckedReason] = useState<string[]>([]);
+  const [date, setDate] = useState<string>("");
   const messagesEndRef = useRef<HTMLDivElement>(null); // 스크롤을 위한 ref
   const handleMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value);
+  };
+
+  const onChangeCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const isChecked = e.target.checked;
+    const value = e.target.id;
+    if (isChecked) {
+      setCheckedReason([...checkedReason, value]);
+    } else {
+      setCheckedReason(checkedReason.filter((reason) => reason !== value));
+    }
+  };
+
+  const handleDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(e.target.value);
+  };
+
+  const handleReportSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (checkedReason.length === 0) {
+      alert("Please select a reason");
+      return;
+    }
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_SERVER_HOST}:${
+          import.meta.env.VITE_SERVER_PORT
+        }/admin/report`,
+        {
+          reporterId: userId,
+          reporteeId: chatParter?.id,
+          reason: JSON.stringify(checkedReason),
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+          withCredentials: true,
+        }
+      );
+      if (res.status === 201) {
+        alert("Reported successfully");
+        location.reload();
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -69,7 +131,10 @@ const ChatRoom = ({}: ChatMessageProps) => {
         },
       })
       .then((res) => {
-        setChatPartner(res.data.result.Users[0].Profile.nickname);
+        setChatPartner({
+          id: res.data.result.Users[0].id,
+          nickname: res.data.result.Users[0].Profile.nickname,
+        });
         let messages = res.data.result.Messages.map((message: any) => {
           let time = new Date(message.createdAt);
           time.setHours(time.getHours() + 9);
@@ -89,8 +154,59 @@ const ChatRoom = ({}: ChatMessageProps) => {
     <>
       <div className="flex flex-col h-screen">
         <Header />
-        <div className="text-center font-mono text-3xl font-bold">
-          {chatParter}
+        <div className="flex justify-between items-center px-4 py-2 font-mono text-3xl font-bold w-auto">
+          <div className="flex-1 text-center">{chatParter?.nickname}</div>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="text-2xl">
+                <AiFillAlert />
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px] bg-white">
+              <DialogHeader>
+                <DialogTitle>Report</DialogTitle>
+              </DialogHeader>
+              <form onSubmit={handleReportSubmit}>
+                <div className="grid gap-4 py-4">
+                  <div className="grid items-center gap-4">
+                    <label htmlFor="swearing">
+                      <input
+                        id="swearing"
+                        type="checkbox"
+                        onChange={onChangeCheckbox}
+                      />
+                      <span className="text-sm"> Swearing(욕설)</span>
+                    </label>
+                    <label htmlFor="dirty">
+                      <input
+                        id="dirty"
+                        type="checkbox"
+                        onChange={onChangeCheckbox}
+                      />
+                      <span className="text-sm"> Dirty talk(음담패설)</span>
+                    </label>
+                  </div>
+                </div>
+                {/* <div className="grid gap-4 py-4">
+                  <div className="grid items-center gap-4">
+                    <label htmlFor="date">Date(eg. 211231 23 59)</label>
+                    <input
+                      id="date"
+                      placeholder="YYMMDD Hour Minute"
+                      value={date}
+                      className="col-span-3"
+                      maxLength={12}
+                      onChange={handleDate}
+                    />
+                  </div>
+                </div> */}
+                <DialogFooter>
+                  <button>Report</button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
         </div>
         <main className="flex flex-col w-full flex-1 px-20 text-center overflow-scroll ">
           <br />
@@ -162,4 +278,5 @@ const ChatRoom = ({}: ChatMessageProps) => {
     </>
   );
 };
+
 export default ChatRoom;
